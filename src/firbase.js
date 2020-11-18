3ade634c4ac775e46ea720720ca49fcbce83b6f9
Dawood <dawood.ali.16.virgo@gmail.com>
2020-11-19 02:18:08 +0500

    headers + css

---
+++ src/firbase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBXKVqv1dl7_2ujcNU-0_5NKUrsiuueQBw",
+  authDomain: "clone-3cf3c.firebaseapp.com",
+  databaseURL: "https://clone-3cf3c.firebaseio.com",
+  projectId: "clone-3cf3c",
+  storageBucket: "clone-3cf3c.appspot.com",
+  messagingSenderId: "908552473254",
+  appId: "1:908552473254:web:6fcbd4382284f40bd24267",
+  measurementId: "G-9DYYYNHM62"
+};
